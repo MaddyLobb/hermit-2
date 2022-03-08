9067300c155293ca9550bff57e83539680c91148
MaddyLobb <81719007+MaddyLobb@users.noreply.github.com>
2022-03-08 12:22:57 -0500

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,36 @@
 //import { gsap } from "gsap";
+//import { waveColorTL } from "./waveColor";
+
+//Darkmode//
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e){
+
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme',dark);
+        ///waveColorTL.play();
+    }
+
+    else{
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme',light); 
+        //waveColorTL.reverse();
+    }
+    
+    console.log(toggleSwitch.checked +'this is the value for the checkbox');
+
+
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') :null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+    }
+}
---
+++ dev/js/waveColor.js
@@ -0,0 +1,31 @@
+// import { gsap } from "gsap";
+
+
+
+// export const waveColorTL = new gsap.timeline({paused:true});
+
+
+
+// const tanWaveTL = new gsap.timeline();
+// tanWaveTL.to("#tanWave2",{duration:0.05, background:"000", color:"000"});
+
+// const orangeWaveTL = new gsap.timeline();
+// orangeWaveTL.to("#orangeWave",{duration:0.05, background:"#000", color:"000"});
+
+// const pinkWaveTL = new gsap.timeline();
+// pinkWaveTL.to("#pinkWave",{duration:0.05, background:"#000", color:"000"});
+
+// const purpleWaveTL = new gsap.timeline();
+// purpleWaveTL.to("#purpleWave",{duration:0.05, background:"#000", color:"000"});
+
+// const darkPurpleWaveTL = new gsap.timeline();
+// darkPurpleWaveTL.to("#darkPurpleWave",{duration:0.05, background:"#000", color:"000"});
+
+
+
+// waveColorTL.add(tanWaveTL, "wave")
+//             .add(orangeWaveTL, "wave")
+//             .add(pinkWaveTL, "wave")
+//             .add(purpleWaveTL, "wave")
+//             .add(darkPurpleWaveTL, "wave")
+
