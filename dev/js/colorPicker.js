ed57209478d6d47c5e374ac52c7e163ad54f6fa7
MaddyLobb <81719007+MaddyLobb@users.noreply.github.com>
2022-03-09 00:36:09 -0500

    color-picker

--- dev/js/colorPicker.js
+++ dev/js/colorPicker.js
@@ -1,6 +1,6 @@
 //import { gsap } from "gsap";
 
-var colorArray = ["#8ecae6","#219ebc","#023047","#ffb703","#fb8500"];
+var colorArray = ["#E7488F","#FF9900","#784F74","#EB6018","#D0B6E0","#D44C4C","#F366CC","#E7943B","#5D2397","#F58251","#A264D2","#FA8787"];
 var colorButtons = document.querySelectorAll(".colors-svg-li");
 var leftShell = document.querySelector("#main-shell-left");
 var rightShell = document.querySelector("#main-shell-right");
