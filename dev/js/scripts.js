574b4f7c8bc474f90704248c0a756325a5d87533
MaddyLobb <81719007+MaddyLobb@users.noreply.github.com>
2022-03-10 21:10:38 -0500

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -50,9 +50,11 @@ const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
+    waveTL.reverse();
 
     if (currentTheme === 'dark'){
         toggleSwitch.checked = true;
+        waveTL.play();
     }
 }
 
--- dev/js/waveColor.js
+++ dev/js/waveColor.js
@@ -2,11 +2,11 @@ import { gsap } from "gsap";
 
 export const waveTL = new gsap.timeline({paused:true});
 
-waveTL.to("#tanWave-2",{duration:0.05, fill:"#000"})
-           .to("#orangeWave-2",{duration:0.05, fill:"#000"})
-           .to("#pinkWave-2",{duration:0.05, fill:"#000"})
-           .to("#purpleWave-2",{duration:0.05, fill:"#000"})
-           .to("#darkPurpleWave-2",{duration:0.05, fill:"#000"});
+waveTL.to("#tanWave-2",{duration:0.05, fill:"#DDB68B"})
+           .to("#orangeWave-2",{duration:0.05, fill:"#DC9C7C"})
+           .to("#pinkWave-2",{duration:0.05, fill:"#A54E5D"})
+           .to("#purpleWave-2",{duration:0.05, fill:"#722B4A"})
+           .to("#darkPurpleWave-2",{duration:0.05, fill:"#22152F"});
 
 // const orangeWaveTL = new gsap.timeline();
 // orangeWaveTL.to("#orangeWave",{duration:0.05, background:"#000", color:"000"});
